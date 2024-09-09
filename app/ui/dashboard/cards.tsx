10479d877bbb0af7a3aaf519f65c1c5cd09cbea2
Jose Ivan <cervantesmartinezjoseivan@gmail.com>
2024-09-09 16:39:34 -0600

    feat: added dashboard functions, added invoices functionality

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,30 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
+import { fetchCardData } from '@/app/lib/data';
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  return (
+
+    
+    <>
+
+    
+      {/* NOTE: Uncomment this code in Chapter 9 */}
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+        />
     </>
   );
 }
